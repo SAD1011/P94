1d5f645caf536bd84fbe37da4cec2701bccfe171
SAD1011 <77442071+SAD1011@users.noreply.github.com>
2021-12-28 17:42:34 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBnJSr5uzlVdpatONq6wJDW30gIIe2iwY8",
+    authDomain: "kwitter-2-389f2.firebaseapp.com",
+    databaseURL: "https://kwitter-2-389f2-default-rtdb.firebaseio.com",
+    projectId: "kwitter-2-389f2",
+    storageBucket: "kwitter-2-389f2.appspot.com",
+    messagingSenderId: "305743204119",
+    appId: "1:305743204119:web:ded7f15a818693358df152"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+   
